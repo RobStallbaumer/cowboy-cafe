8008d2a7d82889b568c38881781d4b2722ddd15a
robby stallbaumer <robstallbaumer@gmail.com>
2020-02-19 14:34:27 -0600

    buttons created

--- PointOfSale/OrderControl.xaml.cs
+++ PointOfSale/OrderControl.xaml.cs
@@ -20,9 +20,105 @@ namespace PointOfSale
     /// </summary>
     public partial class OrderControl : UserControl
     {
+        public List<string> OrderList;
         public OrderControl()
         {
             InitializeComponent();
+            OrderList = new List<string>();
+        }
+
+        private void AddAngryChickenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Angry Chicken");
+        }
+
+        private void AddCowpokeChiliButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Cowpoke Chili");
+
+        }
+
+        private void AddDakotaDoubleBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Dakota Double Burger");
+
+        }
+
+        private void AddPecosPulledPorkButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Pecos Pulled Pork");
+
+        }
+
+        private void AddRustlersRibsButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Rustlers Ribs");
+
+        }
+
+        private void AddTexasTripleBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Texas Triple Burger");
+
+        }
+
+        private void AddTrailBurgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Trail Burger");
+
+        }
+
+        private void AddBakedBeansButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Baked Beans");
+
+        }
+
+        private void AddChiliCheeseFriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Chili Cheese Fries");
+
+        }
+
+        private void AddCornDodgersButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Corn Dodgers");
+
+        }
+
+        private void AddPanDeCampoButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Pan De Campo");
+
+        }
+
+        private void AddCowboyCoffeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Cowboy Coffee");
+
+        }
+
+        private void AddJerkedSodaButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Jerked Soda");
+
+        }
+
+        private void AddTexasTeaButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Texas Tea");
+
+        }
+
+        private void AddWaterButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderList.Add("Water");
+
+        }
+
+        private void OrderListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 }
