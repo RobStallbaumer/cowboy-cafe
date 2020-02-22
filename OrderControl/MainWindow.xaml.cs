fb31b4af73eb017259c24b87cfaf796b47e9f34f
robby stallbaumer <robstallbaumer@gmail.com>
2020-02-22 15:31:00 -0600

    fixed the .net to .core on windows

--- OrderControl/MainWindow.xaml.cs
+++ OrderControl/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PointOfSale
+namespace OrderControl
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
